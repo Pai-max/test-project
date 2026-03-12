857ac1ce3f439dfd177f29fe2f69b758df3ae728
Pai-max <ravindra@tsquaredc.com>
2026-03-12 11:01:04 +0000

    Add Java hello world example

---
+++ Main.java
@@ -0,0 +1,10 @@
+// Defines a class named Main.
+public class Main {
+
+    // This is the main method where the program starts running.
+    public static void main(String[] args) {
+
+        // Prints "Hello, World!" to the console.
+        System.out.println("Hello, World!");
+    }
+}
